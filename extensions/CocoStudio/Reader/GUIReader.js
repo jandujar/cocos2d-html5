0b6b217a5a6a9bfef1890014032a90d68a6f717d
Jose Antonio Andújar Clavell <jandujar@jandujar.com>
2014-02-20 11:33:32 +0100

    GUIReader ya carga ScrollViewCircular

--- extensions/CocoStudio/Reader/GUIReader.js
+++ extensions/CocoStudio/Reader/GUIReader.js
@@ -800,7 +800,11 @@ ccs.WidgetPropertiesReader0300 = ccs.WidgetPropertiesReader.extend({
             widget = ccs.LoadingBar.create();
             this.setPropsForLoadingBarFromJsonDictionary(widget, uiOptions);
         } else if (classname == "ScrollView") {
-            widget = ccs.ScrollView.create();
+            if(uiOptions["tag"] == 69){
+                widget = ccs.ScrollViewCircular.create();
+            }else{
+                widget = ccs.ScrollView.create();
+            }
             this.setPropsForScrollViewFromJsonDictionary(widget, uiOptions);
         }
         else if (classname == "TextArea") {
